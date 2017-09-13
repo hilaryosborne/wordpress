a548ffcccac9324ed1580ffb2c275fbfb0c25f7d
Unknown <h.t.osborne@gmail.com>
2017-09-14 09:58:14 +1000

    Added sentry and acf pro keyless install

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -11,3 +11,17 @@ if (file_exists($autoload = __DIR__.DS.'vendor'.DS.'autoload.php')) {
 } else {
 	throw new \Exception('No autoloader found, please re-run composer');
 }
+
+// OPTIONAL SENTRY SETUP
+// If a sentry URL was provided then setup sentry
+// Sentry is a error reporting service https://sentry.io
+if (getenv('SENTRY_URL')) {
+	// Create a new sentry client
+	$sentry_client = new Raven_Client(getenv('SENTRY_URL'));
+	// Create a new error handler
+	$error_handler = new Raven_ErrorHandler($sentry_client);
+	// Register the recommended handlers
+	$error_handler->registerExceptionHandler();
+	$error_handler->registerErrorHandler();
+	$error_handler->registerShutdownFunction();
+}
