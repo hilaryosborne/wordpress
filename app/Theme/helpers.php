1d6a5347582948c40c67f00eb3a90c073b799e63
Andris Trieb <andris@lambagency.com.au>
2017-09-17 17:00:17 +1000

    Expanded get_partial helper function to work with params as second argument

--- app/Theme/helpers.php
+++ app/Theme/helpers.php
@@ -2,9 +2,9 @@
 
 use App\Theme\Partial;
 
-function get_partial($context, $file=false, $params=[]) {
+function get_partial($context, $fileOrParams=false, $params=[]) {
 
-  $partial = new Partial($context,$file);
+  $partial = new Partial($context,!is_array($fileOrParams) ? $fileOrParams : false);
+  return $partial->get(is_array($fileOrParams) ? $fileOrParams : $params);
 
-  return $partial->get($params);
 }
