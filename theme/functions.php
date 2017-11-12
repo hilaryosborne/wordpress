5886ab07e1072bcddc447865eb6d7abc8c9a017c
Hilary Osborne <h.t.osborne@gmail.com>
2017-11-12 21:15:14 +1000

    Added admin show/hide hook

--- theme/functions.php
+++ theme/functions.php
@@ -1,6 +1,10 @@
 <?php
 
-
+/**
+ * Setup Hook
+ * Call provider inits here
+ * Use this for model inits and any other providers you create
+ */
 function boilerplate_setup() {
     // Hide the admin bar
 	show_admin_bar(false);
@@ -10,6 +14,26 @@ function boilerplate_setup() {
 
 add_action('init', 'boilerplate_setup');
 
+/**
+ * Admin Menu Hook
+ * Use this to configure what wordpress menu items you want to show/hide
+ */
+function boilerplate_admin() {
+    // Configure these depending on what you want to show/hide
+    //remove_menu_page( 'edit-comments.php' );
+    //remove_menu_page( 'upload.php' );
+    //remove_menu_page( 'edit.php' );
+    //remove_menu_page( 'tools.php' );
+    //remove_menu_page( 'plugins.php' );
+    //remove_menu_page( 'themes.php' );
+}
+
+add_action('admin_menu', 'boilerplate_admin');
+
+/**
+ * Mailer Hook
+ * Used to get mailer env vars to work with WordPress mailer object
+ */
 function boilerplate_mailer(PHPMailer $mailer) {
     // Override the default email host
     if (MAIL_HOST) { $mailer->Host = MAIL_HOST; }
