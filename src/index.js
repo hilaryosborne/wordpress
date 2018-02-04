0adbd54be13a31d7d7e9ee7e0693963de7585da4
Hilary Osborne <h.t.osborne@gmail.com>
2018-02-05 09:34:51 +1000

    Updated to use bootstrap official release, made local mysql docker easier to connect to externally and added fontawesome 5 local files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './scss/main.scss';
 import './scripts/main';
 
+require('./css/font-awesome/css/fontawesome-all.min.css');
 require('./css/fonts.css');
 
-require('font-awesome/css/font-awesome.css');
 require('bootstrap/dist/js/bootstrap.bundle.min');
